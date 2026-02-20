ab136e608fa18f0eaa6dc3babf2c13f7392efe80
kenexman <72775018+kenexman@users.noreply.github.com>
2026-02-20 16:14:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Enhanced Backend API Server with File Uploads and Flexible Scheduling
 // server.js
-
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2/promise');
 const bcrypt = require('bcrypt');
